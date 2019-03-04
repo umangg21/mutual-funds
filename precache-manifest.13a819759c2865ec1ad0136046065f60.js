d785324516afb74817b2b73a8bc570f8c4544709
umangg21 <umangg21@gmail.com>
2019-03-04 17:54:23 +0530

    Updates

---
+++ precache-manifest.13a819759c2865ec1ad0136046065f60.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bc4e79fddf08f2c1453",
+    "url": "/mutual-funds/static/js/runtime~main.0bc4e79f.js"
+  },
+  {
+    "revision": "5c603d8216938ccca65d",
+    "url": "/mutual-funds/static/js/main.5c603d82.chunk.js"
+  },
+  {
+    "revision": "19a41cc29bebdd5affd5",
+    "url": "/mutual-funds/static/js/2.19a41cc2.chunk.js"
+  },
+  {
+    "revision": "97c28b53ad370840380787ad6cf67126",
+    "url": "/mutual-funds/index.html"
+  }
+];
